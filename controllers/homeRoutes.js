081e760e44720f20e4d0010897669fef10bc1732
VoravichS <vsilapac@gmail.com>
2022-08-30 11:08:32 -0400

    merge

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,14 @@ const router = require("express").Router();
 const { User } = require("../models");
 const withAuth = require("../utils/auth");
 
+// Landing Page
+router.get("/", async (req, res) => {
+    res.render("homepage", {
+      blogs,
+      logged_in: req.session.logged_in
+    });
+});
+
 router.get("/login", (req, res) => {
   res.render("login");
 });
