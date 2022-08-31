575a8056eed3e63c0ef88e1d2e51ee051aacbd11
VoravichS <vsilapac@gmail.com>
2022-08-31 11:22:37 -0400

    saving before merge

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,9 +4,7 @@ const { User } = require("../../models");
 //* Signup New User
 router.post("/", async (req, res) => {
   try {
-    console.log(req.body);
     const userData = await User.create(req.body);
-    console.log(userData);
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.is_dm = userData.is_dm;
