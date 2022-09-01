8d15da9bdce2bb376839faf5019ed28d5cf9bfbd
aaansari123 <asadali1029@gmail.com>
2022-09-01 15:25:11 -0400

    working on adding char functionality

---
+++ public/js/charCreate.js
@@ -0,0 +1,24 @@
+const charFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const charName = document.querySelector("#name").value.trim();
+    const className = document.querySelector("#class-select").value.trim();
+    const raceName = "human"
+  
+    if (charName && className) {
+      const response = await fetch("/api/characters", {
+        method: "POST",
+        body: JSON.stringify({ charName, className, raceName  }),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.ok) {
+        response.status(200).json(response);
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  document
+    .querySelector("#charSubmitBtn")
+    .addEventListener("click", charFormHandler);
