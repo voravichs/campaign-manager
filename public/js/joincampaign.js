158a245270ec6f0e4ba9e0b63c0bf64ddf50e16f
VoravichS <vsilapac@gmail.com>
2022-09-01 15:24:50 -0400

    add authorization to paths, finish join functionality

--- public/js/joincampaign.js
+++ public/js/joincampaign.js
@@ -3,19 +3,19 @@ const joinHandler = async (event) => {
 
     const campaignID = document.location.href.split("/")[document.location.href.split("/").length - 1];
     const characterID = document.querySelector("#join-character").value;
-    console.log(JSON.stringify({ campaignID }));
+    console.log(JSON.stringify({ "campaign_id": Number(campaignID) }));
 
     const response = await fetch("/api/characters/"+characterID, {
         method: "PUT",
-        body: JSON.stringify({ campaignID }),
+        body: JSON.stringify({ "campaign_id": Number(campaignID) }),
         headers: { "Content-Type": "application/json" },
     });
-    
-// if (response.ok) {
-//     document.location.replace("/profile/dm/"+campaignID);
-// } else {
-//     alert(response.statusText);
-// }
+
+    if (response.ok) {
+        document.location.replace("/profile/dm/"+campaignID);
+    } else {
+        alert(response.statusText);
+    }
 };
 
 if (document.querySelector("#join-btn")) {
