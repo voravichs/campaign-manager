889db71ea0ed489ff73fa1e2cfd778d4eaae123b
aaansari123 <asadali1029@gmail.com>
2022-09-01 15:56:30 -0400

    saving before integration

--- public/js/charCreate.js
+++ public/js/charCreate.js
@@ -6,11 +6,14 @@ const charFormHandler = async (event) => {
     const raceName = "human"
   
     if (charName && className) {
+        console.log(charName);
+        console.log(className);
       const response = await fetch("/api/characters", {
         method: "POST",
-        body: JSON.stringify({ charName, className, raceName}),
+        // body: JSON.stringify({ charName, className, raceName}),
         // headers: { "Content-Type": "application/json" },
       });
+      console.log(response);
       if (response.ok) {
         response.status(200).json(response);
       } else {
