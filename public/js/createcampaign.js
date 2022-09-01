5dceb31b4a5a2d2105a567fc561abb1668df340e
zym0tic <lukerozon@gmail.com>
2022-09-01 16:00:05 -0400

    saving to pull

--- public/js/createcampaign.js
+++ public/js/createcampaign.js
@@ -1,25 +1,16 @@
-const createCampaign = async (event) => {
+const createCampaignHandler = async (event) => {
     event.preventDefault();
 
     const campaignName = document.querySelector("#campaignName").value.trim();
 
-//if (username && password) {
-//    const response = await fetch("/api/users/login", {
-//        method: "POST",
-//        body: JSON.stringify({ username, password }),
-//        headers: { "Content-Type": "application/json" },
-//    });
-//
-//    if (response.ok) {
-//        document.location.replace("/");
-//    } else {
-//        alert(response.statusText);
-//    }
-//}
-};
+    const response = await  fetch(`api/campaign`, {
+        method: 'POST',
+        body: JSON.stringify({campaignName})
+    });
 
-if (document.querySelector("#join-btn")) {
-    document
-        .querySelector("#join-btn")
-        .addEventListener("click", joinHandler);
-}
+    if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to create campaign');
+      }
+};
