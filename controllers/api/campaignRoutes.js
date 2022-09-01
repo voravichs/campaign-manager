8d20b13a577979dc8891f6851ca99cff808649b9
aaansari123 <asadali1029@gmail.com>
2022-09-01 11:21:30 -0400

    saving before integration

--- controllers/api/campaignRoutes.js
+++ controllers/api/campaignRoutes.js
@@ -6,7 +6,7 @@ router.get('/', async (req, res) => {
 
   try {
     const campaignData = await Campaign.findAll();
-    const campagins = campaignData.map((campaign) => campaign.get({plain: true}));
+    const campaigns = campaignData.map((campaign) => campaign.get({plain: true}));
     res.status(200).json(campaigns);
   } catch (error) {
     res.status(500).json(error);
